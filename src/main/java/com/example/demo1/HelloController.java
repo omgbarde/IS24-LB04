9cd7829207b04ee667e7a9b69e280bf5b815685d
Piz <pietro.pizzoccheri@gmail.com>
2024-03-03 17:23:16 +0100

    test commit & push by Pitesse

--- src/main/java/com/example/demo1/HelloController.java
+++ src/main/java/com/example/demo1/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Lesgoski, leasgo sì è così!");
     }
 }
