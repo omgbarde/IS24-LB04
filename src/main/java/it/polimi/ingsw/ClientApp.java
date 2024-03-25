956163941c4161d6486f5cf73cce35bdc2e13977
lorenzobardelli <lorenzo.bardelli@n-3.it>
2024-03-25 11:48:17 +0100

    added gson library + example in ClientApp

--- src/main/java/it/polimi/ingsw/ClientApp.java
+++ src/main/java/it/polimi/ingsw/ClientApp.java
@@ -1,4 +1,10 @@
 package it.polimi.ingsw;
 
+import com.google.gson.Gson;
+
 public class ClientApp {
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+        //String json = gson.toJson(oggetto);
+    }
 }
