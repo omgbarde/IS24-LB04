408ba79ecdb72619bfdaa846d868d2fa940e1742
AlexIlLeone2 <alex.hathaway@mail.polimi.it>
2024-05-15 17:04:00 +0200

    Drawing board CLI

--- src/main/java/codex/lb04/View/Cli/CardRenderer.java
+++ src/main/java/codex/lb04/View/Cli/CardRenderer.java
@@ -38,11 +38,125 @@ public class CardRenderer {
     }
 
     /**
-     * This method converts a card to a string representation to be printed in CLI mode
-     * @param card the card to be converted
-     * @return the string representation of the card
+     * This method converts resource types to emojis
+     * @param ID the id of the card
+     * @return the emoji representation of the multiplier
      */
-    public static String cardToString(Card card) {
+    private static String multiplierMap(Integer ID) {
+        switch (ID) {
+            case 41, 51, 63, 71:
+                return "\uD83E\uDEB6"; //quill
+            case 42, 53, 61, 73:
+                return "\uD83E\uDED9"; //inkwell
+            case 43, 52, 62, 72:
+                return "\uD83D\uDCDC"; //manuscript
+            case 44, 45, 46, 54, 55, 56, 64, 65, 66, 74, 75, 76:
+                return "⬛"; //angle
+            default:
+                return "  ";
+        }
+    }
+
+    //non verrà usato
+    public String printInGame(Card card){
+        String[] components = renderIngame(card);
+        StringBuilder stringBuilder = new StringBuilder();
+        for(String s:components){
+            stringBuilder.append(s);
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
+    }
+
+    public static String[] renderIngame(Card card){
+        StringBuilder stringBuilder = new StringBuilder();
+        //get the corners of the card
+        Corner upperLeft = card.getShownFace().getUpperLeft();
+        Corner upperRight = card.getShownFace().getUpperRight();
+        Corner lowerRight = card.getShownFace().getLowerRight();
+        Corner lowerLeft = card.getShownFace().getLowerLeft();
+        ArrayList<ResourceType> centralResources = card.getShownFace().getCentralResources();
+
+        int padding = 3 - centralResources.size();
+
+        String[] componentsArray = new String[3];
+        //upper part    
+        if (!upperLeft.isCovered()){
+            ResourceType resource = upperLeft.getResource();
+            if(resource!=null)stringBuilder.append(resourceMap(resource));
+            else stringBuilder.append("⬜");
+        }else stringBuilder.append("⬛");
+
+        stringBuilder.append("----");
+
+        if (!upperRight.isCovered()){
+            ResourceType resource = upperRight.getResource();
+            if(resource!=null)stringBuilder.append(resourceMap(resource));
+            else stringBuilder.append("⬜");
+        }else stringBuilder.append("⬛");
+
+        componentsArray[0] = stringBuilder.toString();
+        
+        stringBuilder = new StringBuilder();
+        stringBuilder.append("|");
+        //middle part
+        for(int i = 0; i < padding; i++ ){
+            stringBuilder.append(" ");
+        }
+        for(ResourceType r:centralResources){
+            stringBuilder.append(resourceMap(r));
+        }
+        for(int i = 0; i < padding; i++ ){
+            stringBuilder.append(" ");
+        }
+        stringBuilder.append("|");
+
+        componentsArray[1] = stringBuilder.toString();
+
+        stringBuilder = new StringBuilder();
+
+        //lower part
+        if (!lowerLeft.isCovered()){
+            ResourceType resource = lowerLeft.getResource();
+            if(resource!=null)stringBuilder.append(resourceMap(resource));
+            else stringBuilder.append("⬜");
+        }else stringBuilder.append("⬛");
+
+        stringBuilder.append("----");
+
+        if (!lowerRight.isCovered()){
+            ResourceType resource = lowerRight.getResource();
+            if(resource!=null)stringBuilder.append(resourceMap(resource));
+            else stringBuilder.append("⬜");
+        }else stringBuilder.append("⬛");
+
+        componentsArray[2] = stringBuilder.toString();
+
+        return componentsArray;
+    }
+
+    public static String[] placeHolder(){
+        String[]components = new String[3];
+
+        components[0] = " ------ ";
+        components[1] = "|      |";
+        components[2] = " ------ ";
+
+        return components;
+    }
+
+    //non verrà usato
+    public String printInHand(Card card){
+        String[] components = renderInHand(card);
+        StringBuilder stringBuilder = new StringBuilder();
+        for(String s:components){
+            stringBuilder.append(s);
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
+    }
+
+    public static String[] renderInHand(Card card){
         StringBuilder cardString = new StringBuilder();
         ArrayList<Integer> resourcesNeededArray = new ArrayList<>();
 
@@ -126,4 +240,5 @@ public class CardRenderer {
         costArray.add(card.getLeaf_needed());
         return costArray;
     }
+
 }
